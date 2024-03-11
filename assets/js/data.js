33ba90e8a42066311103835749a21e990e9df641
teenage1ove <kirillstars7@gmail.com>
2024-03-11 13:59:14 +0300

    Add audio description

---
+++ assets/js/data.js
@@ -0,0 +1,34 @@
+export const data = [
+    {
+        id: 1,
+        link: "beat1.mp3",
+        genre: "Rock",
+        track: "Enemy",
+        group: "Imagine Dragons",
+        year: 2019,
+    },
+    {
+        id: 2,
+        link: "beat2.mp3",
+        genre: "Indie rock",
+        track: "Next to me",
+        group: "Imagine Dragons",
+        year: 2021,
+    },
+    {
+        id: 3,
+        link: "beat3.mp3",
+        genre: "Rap",
+        track: "Rammlied",
+        group: "Rammstein",
+        year: 2009,
+    },
+    {
+        id: 4,
+        link: "beat4.mp3",
+        genre: "Rap",
+        track: "The search",
+        group: "NF",
+        year: 2018,
+    },
+]
