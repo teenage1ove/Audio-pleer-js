bb4c16833d9d046bfaa4773f425932b04fc4f57d
teenage1ove <kirillstars7@gmail.com>
2024-03-12 20:47:23 +0300

    Add functions for play music

---
+++ assets/js/utils.js
@@ -0,0 +1,10 @@
+const formatTime = (time) => {
+    return time < 10 ? `0${time}` : time
+}
+
+export const toMinAndSec = (duration) => {
+    const minutes = formatTime( Math.floor(duration / 60) )
+    const seconds = formatTime( Math.floor(duration - minutes * 60) )
+
+    return `${minutes}:${seconds}`
+}
